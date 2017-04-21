b7cd4717a67e116d9b741b446cd77a60e1842e88
WILLIAM-OBRIEN <C15396226@gmail.com>
2017-04-21 23:27:35 +0100

    Process of adding new instance.
    Adds new instance for calorie counter.

---
+++ app/src/main/java/com/example/williamobrien/pocketfitness/Display.java
@@ -0,0 +1,17 @@
+package com.example.williamobrien.pocketfitness;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by William on 21/04/2017.
+ */
+
+public class Display extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+
+}
