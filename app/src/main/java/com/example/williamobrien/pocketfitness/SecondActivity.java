fd1234243ecd27165923699d07ade9f2bb766b19
WILLIAM-OBRIEN <C15396226@gmail.com>
2017-04-23 14:35:56 +0100

    Adding input fields.
    Adding input fields for calorie counter activity.

--- app/src/main/java/com/example/williamobrien/pocketfitness/SecondActivity.java
+++ app/src/main/java/com/example/williamobrien/pocketfitness/SecondActivity.java
@@ -2,12 +2,22 @@ package com.example.williamobrien.pocketfitness;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SecondActivity extends ActionBarActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.exercise,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        
     }
 }
