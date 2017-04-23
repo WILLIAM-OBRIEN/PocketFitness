6396220547860610ec3828d1bcda857a28dc7094
WILLIAM-OBRIEN <C15396226@gmail.com>
2017-04-23 17:44:41 +0100

    Saves input field and form submit button.
    Gives error message for empty name field and stores user input in list
    array.

--- app/src/main/java/com/example/williamobrien/pocketfitness/SecondActivity.java
+++ app/src/main/java/com/example/williamobrien/pocketfitness/SecondActivity.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends ActionBarActivity {
 
     Spinner spinner;
     ArrayAdapter<CharSequence> adapter;
+    Button submitCal;
+    ArrayList<String> add_cal = new ArrayList<String>();
+    EditText txt;
+    ListView show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,26 @@ public class SecondActivity extends ActionBarActivity {
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
+        txt = (EditText)findViewById(R.id.name);
+        submitCal = (Button)findViewById(R.id.cal_sub);
+        submitCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getInput = txt.getText().toString();
+
+                if (getInput==null || getInput.trim().equals(""))
+                {
+                    Toast.makeText(getBaseContext(),"Input field is empty",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    add_cal.add(getInput);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(SecondActivity.this,android.R.layout.simple_list_item_1,add_cal);
+                    show.setAdapter(adapter);
+                    ((EditText)findViewById(R.id.name)).setText(" ");
+                }
             }
         });
     }
