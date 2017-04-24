5c91bdf780e1e1c1cb6d1b264ff4588babbca530
ButBlamo <daniel.moloney45@gmail.com>
2017-04-24 12:00:55 +0100

    Added onclick for submit button and code for calculating bmi

--- app/src/main/java/com/example/williamobrien/pocketfitness/MainActivity.java
+++ app/src/main/java/com/example/williamobrien/pocketfitness/MainActivity.java
@@ -7,12 +7,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.text.TextUtils;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public Button but1;
+    //Takes user to BMI calculation page when pressed
     public Button but2;
+    //BMI calculator input button
+    public Button but3;
+
+    //The three inputted values in BMI calc
+    EditText name = (EditText) findViewById(R.id.NameInput);
+    EditText weight = (EditText) findViewById(R.id.WeightInput);
+    TextView height = (TextView) findViewById(R.id.HeightInput);
 
     public void init()
     {
@@ -67,5 +78,84 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Calculating bmi and returning text based on the value calculated
+    public void bmiErrorCheck()
+    {
+        but3 = (Button) findViewById(R.id.BMIButton);
+
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //Float variables for taking in the values of user height and weight
+                float weightKg;
+                float heightCm;
+                float bmiCalculated;
+
+                //Putting user inputted information into strings
+                String username = name.getText().toString();
+                String userWeight = weight.getText().toString();
+                String userHeight = height.getText().toString();
+
+                //Error checking input in BMI page to make sure there is input
+                //There will be a message sent to the box if empty
+                if (TextUtils.isEmpty(username))
+                {
+                    name.setError("Please enter your name");
+                    name.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userWeight))
+                {
+                    weight.setError("Please enter your weight");
+                    weight.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userHeight))
+                {
+                    height.setError("Please enter your height");
+                    height.requestFocus();
+                    return;
+                }
+
+                //Parsing inputted values in height and weight
+                weightKg = Float.parseFloat(userWeight);
+                heightCm = Float.parseFloat(userHeight);
+
+                //Reference to method CalculateBmi function
+                bmiCalculated = calculateBmi(weightKg, heightCm);
+
+                String determineResponse = interpretBmi(bmiCalculated);
+            }
+
+        });
+    }
+
+
+    //Calculating bmi
+    private Float calculateBmi(float kg, float cm)
+    {
+        float toBeReturned = 0.0f;
+        float heightCalculate = 0.0f;
+
+        toBeReturned = toBeReturned + (kg * .45f);
+
+        heightCalculate = heightCalculate + (cm * .025f);
+
+        heightCalculate = heightCalculate * heightCalculate;
+
+        toBeReturned = toBeReturned / heightCalculate;
+
+        return(toBeReturned);
+    }
+
+
+    private String interpretBmi(float bmiCalculated)
+    {
+        if (bmi)
     }
 }
