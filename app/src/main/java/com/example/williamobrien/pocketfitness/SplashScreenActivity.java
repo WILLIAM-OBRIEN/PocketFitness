e428318a3839d1cbeca0fbc85c4fec692e0f51c7
WILLIAM-OBRIEN <C15396226@gmail.com>
2017-04-26 00:27:59 +0100

    Splash screen start.
    Attempting to add splash screen.

---
+++ app/src/main/java/com/example/williamobrien/pocketfitness/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.williamobrien.pocketfitness;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends ActionBarActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
